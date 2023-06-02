a64420ab5155adde3079b4f65dab5ff5de908e68
Jarjish Siddibapa <jarjishsiddibapa@gmail.com>
2023-06-02 10:14:08 +0530

    Day 4 completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -13,11 +13,52 @@ class LoginPage extends StatelessWidget {
             'assets/images/login_image.png',
             fit: BoxFit.cover,
           ),
-          const Text(
-            'Welcome to Catalog',
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 22,
+          const SizedBox(
+            height: 20,
+          ),
+          const Text('Welcome to Catalog',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 24,
+              )),
+          const SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 16,
+              horizontal: 32,
+            ),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: const InputDecoration(
+                    hintText: 'enter username',
+                    labelText: 'Username',
+                  ),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    hintText: 'enter password',
+                    labelText: 'Password',
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    print('Hi from Jarjish Siddibapa');
+                  },
+                  style: TextButton.styleFrom(
+                    foregroundColor: Colors.white,
+                  ),
+                  child: const Text(
+                    'Login',
+                  ),
+                )
+              ],
             ),
           )
         ],
